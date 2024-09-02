77bde290ea6a543917a0987067caea2505923a30
kirillibrahim <kirillbrah@gmail.com>
2024-09-02 21:35:05 +0300

    Refactor ExamEditor Component & ExamEdior Page

--- src/pages/exam/index.tsx
+++ src/pages/exam/index.tsx
@@ -1,27 +1,11 @@
-import { useRouter } from 'next/router';
 import ExamEditor from '../../components/ExamEditor';
-import { useEffect, useState } from 'react';
-import { Exam } from '../../constants/types';
-import { getExamByIdFromLocalStorage } from '../../utils/localStorageUtils';
 
-const ExamEditorPage = () => {
-  //const router = usePathname();
-  const router = useRouter()
-  const  {id}  = router.query; // Exam ID from query parameter
-  console.log("id", id)
-  const [initialExam, setInitialExam] = useState<Exam | undefined>(undefined);
-
-  useEffect(() => {
-    if (id) {
-      const exam = getExamByIdFromLocalStorage(id as string);
-      setInitialExam(exam);
-    }
-  }, [id]);
+const ExamEditorPage: React.FC = () => {
 
   return (
     <div className="container mx-auto p-6">
     
-      <ExamEditor initialExam={initialExam} />
+      <ExamEditor  />
     </div>
   );
 };
